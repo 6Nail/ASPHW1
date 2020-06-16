4f6a045580eb21580c06be3e26fd02dacb836482
Nail <55398701+6Nail@users.noreply.github.com>
2020-06-16 19:14:24 +0600

    Add files via upload

---
+++ ASP_HW_01/WebApplication2/Controllers/Students2Controller.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Data;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    //localhost/Students2/
+    // Scoped
+    // Transcient
+    // Singleton
+
+    public class Students2Controller : Controller
+    {
+        public IList<Student> _students { get; private set; }
+
+        public Students2Controller()
+        {
+            this._students = new List<Student>()
+            {
+                new Student() {Id = 1, Name = "Student 1", Mark = 4},
+                new Student() {Id = 2, Name = "Student 2", Mark = 4},
+                new Student() {Id = 3, Name = "Student 3", Mark = 2},
+                new Student() {Id = 4, Name = "Student 4", Mark = 3},
+                new Student() {Id = 5, Name = "Student 5", Mark = 5}
+            };
+        }
+
+
+        // GET: Students2
+        public ActionResult Index()
+        {
+            return View(_students);
+        }
+
+        /// <summary>
+        /// https://bitbucket.org/absagatnd/day2/src/master/
+        /// Создать Details.cshtml (View)
+        /// Отобразить данные с переменной student во View
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Details(int id)
+        {
+            Student student = _students.FirstOrDefault(s => s.Id == id);
+            return View(student);
+        }
+
+        // GET: Students2/Create
+        [HttpGet]
+        public ActionResult Create(int id)
+        {
+            return View();
+        }
+
+        // POST: Students2/Create
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _students.Add(student);
+                }
+
+                ViewBag.Count = _students.Count;
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Students2/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var student = _students.FirstOrDefault(s => s.Id == id);
+            if(student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students2/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Student student)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _students[id - 1] = student;
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(student);
+            }
+        }
+
+        // GET: Students2/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Students2/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Student student)
+        {
+            try
+            {
+                student = _students.FirstOrDefault(x => x.Id == id);
+                _students.Remove(student);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(student);
+            }
+        }
+    }
+}
